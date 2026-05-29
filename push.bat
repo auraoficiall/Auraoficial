f63492191cdf2cd05e8baed655d242822ff11d8f
auraoficial1 <andrewjosuev@gmail.com>
2026-05-29 00:26:39 -0400

    update

--- push.bat
+++ push.bat
@@ -1,7 +1,6 @@
 @echo off
 git add -A
-git commit --allow-empty -m "update %date% %time%"
-git push https://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@github.com/auraoficiall/Auraoficial.git main
-echo.
+git commit --allow-empty -m "update"
+git push origin main
 echo Listo! Vercel actualiza en 1 minuto.
 timeout /t 3
