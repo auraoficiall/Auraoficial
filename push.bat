1aa93344cf2194a0d60244694afa1ed0c57abe21
auraoficial1 <andrewjosuev@gmail.com>
2026-05-28 21:34:32 -0400

    update

--- push.bat
+++ push.bat
@@ -1,8 +1,6 @@
 @echo off
-echo Subiendo cambios a GitHub...
 git add .
 git commit -m "update"
 git push https://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@github.com/auraoficiall/Auraoficial.git main
-echo.
-echo Listo! Netlify se actualiza en 1 minuto.
-pause
+echo Listo! Vercel actualiza en 1 minuto.
+timeout /t 3
